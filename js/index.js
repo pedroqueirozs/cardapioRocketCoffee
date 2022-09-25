d8fc882775925e32efd8842591ae7d036c4b977b
joao <joaopaulo.queiroz.545@gmail.com>
2022-09-25 15:40:30 -0300

    adicionado modal

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// alert("Olá ,Seja bem vindo ao RocketCoffee");
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+
+
+
+document.getElementById("form").addEventListener("submit", function(e) {
+e.preventDefault();
+const {name,description,type,price}= e.target
+adicionar(name.value,description.value,type.value,price.value)
+limpar()
+
+})
+
+function limpar(){
+  document.getElementById("nome").value=""
+  document.getElementById("textarea").value=""
+  document.getElementById("preco").value=""
+}
+function adicionar(name,description,type,price) {
+  let ul = document.getElementById(type);
+  let li = document.createElement("li");
+  let div = document.createElement("div");
+  let h3 = document.createElement("h3");
+  let p = document.createElement("p");
+  let strong = document.createElement("strong");
+  h3.innerHTML = name;
+  p.innerHTML = description;
+  strong.innerHTML = 'R$ '+ price;
+  strong.className = "price";
+  div.className = "details";
+
+  div.append(h3);
+  div.append(p);
+
+  li.appendChild(div);
+  li.appendChild(strong);
+  ul.appendChild(li);
+}
